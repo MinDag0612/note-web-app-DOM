c1a6a5677d0d6fc51c757a3c8cf58be24b6c1f6a
Dang <tonminhdang9@gmail.com>
2026-03-17 16:56:31 +0700

    (conf) - add bracnh for deploy phase 2

--- phase3/src/components/ButtonCom.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import "./ButtonCom.css"
-
-export default function ButtonCom({name, type, onClick}) {
-  return (
-    <button type={type} className="btn w-100 btn-styled" onClick={onClick}>{name}</button>
-  )
-}
